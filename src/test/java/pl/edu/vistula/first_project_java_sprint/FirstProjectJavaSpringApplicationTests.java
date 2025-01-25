5b092dde25e107b1b8915aef8162b9686533daf1
Mikołaj <miki03@interia.pl>
2025-01-25 11:17:26 +0100

    Zadanie Spring

---
+++ src/test/java/pl/edu/vistula/first_project_java_sprint/FirstProjectJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
